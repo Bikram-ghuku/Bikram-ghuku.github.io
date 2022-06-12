afddcf66b0087d3824e888a34de68887b804c7dc
Bikram Ghuku <bikramghuku05@gmail.com>
2022-06-12 10:44:49 +0530

    added news certs and updated packages

--- src/cert.js
+++ src/cert.js
@@ -10,5 +10,17 @@ export const certs=[
         name:"HackerRank SQL(Basic)",
         url:"https://www.hackerrank.com/certificates/0274a79c1253",
         imgUrl:"https://i.imgur.com/Ze8utbG.png"
+    },
+    {
+        id: 3,
+        name: "Hackerank Problem Solving (Basic)",
+        url: "https://www.hackerrank.com/certificates/fc64e3ec03d2",
+        imgUrl:"https://imgur.com/3trIjOp.png"
+    },
+    {
+        id:4,
+        name:"Postman student expert",
+        url:"https://api.badgr.io/public/assertions/UrK9J1n6QOiU_yWr8yBfWg",
+        imgUrl:"https://imgur.com/8ZwoW1F.png"
     }
 ]
